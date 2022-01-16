e8d3611379d5f59708c4f0d321e65a23647661ea
Malik M Jahangir <73543873+Malik786-git@users.noreply.github.com>
2022-01-17 02:25:23 +0500

    update files

---
+++ app.js
@@ -0,0 +1,19 @@
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+// key: dd95ddb0ca9df88c759b05bf21e5210e
+const weatherApi = {
+    key: 'dd95ddb0ca9df88c759b05bf21e5210e',
+    baseUrl:'https://api.openweathermap.org/data/2.5/weather'
+}
+function apifun() {
+    
+    // let url = `${weatherApi.baseUrl}?q=Mumbai&appid=${weatherApi.key}`;
+ 
+    fetch(`${weatherApi.baseUrl}?q=London&appid=${weatherApi.key}`)
+    .then(res =>res.json())
+    .then(UpdateUi)
+}
+apifun();
+
+function UpdateUi(data) {
+    console.log(data);
+}
